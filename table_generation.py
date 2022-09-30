18725d72c5f903c9619998687c78faf85f6f07f9
s-emerson <s.emerson@ufl.edu>
2022-09-29 21:55:21 -0400

    Create table_generation file with basic directions outlined

---
+++ table_generation.py
@@ -0,0 +1,8 @@
+# variables: year_of_birth, lived_years, lived_months, lived_days
+
+# 2 tables ranking the top 10 Presidents from longest lived to shortest lived and then the top 10 presidents from shortest lived to longest lived
+# (use CURRENT_DATE via external library to keep track of presidents that are still living)
+
+# also calculate the mean, weighted average, median, mode, max, min and standard deviation of lived_days, put this in a table, and then make a plot that shows the distribution of the data (year_of_birth vs lived_years, maybe? and then put the weighted average vs the actual x-y trend line)
+
+# Assemble your tables and plot into a single report with a title, author and date. Start the report with a 3 or 4 sentence summary of what you found, the body should have your tables and plot and any commentary about your methods or assumptions you think the reader would need to know. Finish the report with a conclusions section, where you state the conclusions you reached through this analysis.
